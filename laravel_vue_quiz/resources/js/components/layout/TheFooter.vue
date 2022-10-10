8040e6374d52884b3ead5afcc1ea05e138a09158
Gen Suzuki <48849186+gsuzuki983@users.noreply.github.com>
2022-10-10 12:11:57 +0000

    finished 4-1

---
+++ laravel_vue_quiz/resources/js/components/layout/TheFooter.vue
@@ -0,0 +1,8 @@
+<template>
+    <footer>
+      <div class="container text-center">
+        <small>© 2020 4Answers-quiz. All rights reserved.</small>
+      </div>
+    </footer>
+  </template>
+  
