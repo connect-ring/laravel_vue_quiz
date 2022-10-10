4c2eb862388128075fe09b48855239020bd0f2a2
Gen Suzuki <48849186+gsuzuki983@users.noreply.github.com>
2022-10-10 12:59:23 +0000

    finished 5-2

--- laravel_vue_quiz/resources/js/app.js
+++ laravel_vue_quiz/resources/js/app.js
@@ -1,10 +1,10 @@
 import Vue from 'vue'
 import router from './router.js'
-import VueSocialSharing from 'vue-social-sharing'
-
+import VueSocialSharing from 'vue-social-sharing';
+import VueCompositionAPI from '@vue/composition-api'
+Vue.use(VueCompositionAPI)
 Vue.use(VueSocialSharing);
-
 new Vue({
   el: '#app',
-  router: router, // routerにはrouter.jsファイルを設定します
- }).$mount('#app') // routerを適用する要素を設定(マウント)します
+  router: router,
+}).$mount('#app')
